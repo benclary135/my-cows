c7e44340952d8ba2ec9d986a9c03099eb4707dae
vagabond <benclary135@gmail.com>
2022-11-19 16:24:23 -0500

    Link mooMethods.js to index.html.  Add myShed and updateScores functions.

--- mooMethods.js
+++ mooMethods.js
@@ -1,34 +1,47 @@
-function Player(cows, cash, victim, temp) {
+function Player(cows, shedCows, cash, victim, temp) {
     this.cows = cows;
+    this.shedCows = shedCows;
     this.cash = cash;
     this.victim = victim;
     this.temp = temp;
 }
 
-let playerOne = new Player(0, 0, false, 0);
-let playerTwo = new Player(0, 0, false, 0);
+let pOne = new Player(0, 0, 0, 0, false, 0);
+let pTwo = new Player(0, 0, 0, 0, false, 0);
+let pOneScore = 0;
+let pTwoScore = 0;
+
+function updateScores() {
+    pOneScore = pOne.cows + pOne.shedCows;
+    pTwoScore = pTwo.cows + pTwo.shedCows;
+}
 
 function myCows(player) {
     player.cows += 1;
     player.victim = false;
+    updateScores();
 }
 
 function mySteeple(player) {
     player.cows *= 2;
+    updateScores();
 }
 
 function myGraveyard(targetedPlayer) {
     targetedPlayer.temp = targetedPlayer.cows;
     targetedPlayer.cows = 0;
     targetedPlayer.victim = true;
+    updateScores();
 }
 
 function myGoats(player) {
-    player.cows = Math.floor(player.cows* 1.5);
+    player.cows = Math.floor(player.cows * 1.5);
+    updateScores();
 }
 
 function myDollarGeneral(player) {
     player.cash += 1;
+    updateScores();
 }
 
 function myAnimalHospital(player) {
@@ -37,5 +50,18 @@ function myAnimalHospital(player) {
     } else {
         player.cows = player.temp;
         player.temp = 0;
+        player.victim = false;
     }
+    updateScores();
 }
+
+function myShed(player) {
+    if (player.cows < 2) {
+        alert("You need at least two cows to fill a shed!");
+    } else {
+        player.shedCows += 2;
+        player.cows -= 2;
+    }
+    updateScores();
+}
+
