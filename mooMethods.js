991e13218f02abc02d9170cf42de798d6153524f
vagabond <benclary135@gmail.com>
2022-11-19 16:58:08 -0500

    Add endGame function and corresponding button.

--- mooMethods.js
+++ mooMethods.js
@@ -1,4 +1,4 @@
-function Player(cows, shedCows, cash, victim, temp) {
+function player(cows, shedCows, cash, victim, temp) {
     this.cows = cows;
     this.shedCows = shedCows;
     this.cash = cash;
@@ -6,8 +6,8 @@ function Player(cows, shedCows, cash, victim, temp) {
     this.temp = temp;
 }
 
-let pOne = new Player(0, 0, 0, 0, false, 0);
-let pTwo = new Player(0, 0, 0, 0, false, 0);
+let pOne = new player(0, 0, 0, 0, false, 0);
+let pTwo = new player(0, 0, 0, 0, false, 0);
 let pOneScore = 0;
 let pTwoScore = 0;
 
@@ -63,5 +63,38 @@ function myShed(player) {
         player.cows -= 2;
     }
     updateScores();
+}
+
+function endGame() {
+    updateScores();
+    if (pOneScore === pTwoScore) {
+        if (pOne.cash > pTwo.cash) {
+            alert("Player one wins by a cash value tie-breaker!!!  What a close game!");
+        } else if (pTwo.cash > pOne.cash) {
+            alert("Player two wins by a cash value tie-breaker!!!  What a close game!");
+        } else if (pOne.cash === pTwo.cash) {
+            alert("It's a true tie!!!  Both players have an equal amount of cows and cash!");
+        } else {
+            alert("Huh?  Something's weird....");
+        }
+    } else if (pOneScore > pTwoScore) {
+        alert("Player One wins with a cow victory!!!");
+    } else if (pTwoScore > pOneScore) {
+        alert("Player Two wins with a cow victory!!!");
+    } else {
+        alert("Huh?  Something's weird...");
+    }
+    pOne.cows = 0;
+    pTwo.cows = 0;
+    pOne.shedCows = 0;
+    pTwo.shedCows = 0;
+    pOne.cash = 0;
+    pTwo.cash = 0;
+    pOne.victim = false;
+    pTwo.victim = false;
+    pOne.temp = 0;
+    pTwo.temp = 0;
+    pOneScore = 0;
+    pTwoScore = 0;
 }
 
